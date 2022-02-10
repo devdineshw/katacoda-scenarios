02add87428cc0146532cb91a6df95dd7eb950c66
dev.dneshw@gmail.com <dev.dineshw@gmail.com>
2022-02-10 17:42:19 +0530

    fixing json issu

--- WSO2-APIM-3.2.0-k8s-scenario/assets/sethostname.sh
+++ WSO2-APIM-3.2.0-k8s-scenario/assets/sethostname.sh
@@ -3,8 +3,9 @@
 MGTURL=`cat kcmgthostname`
 HTTPURL=`cat kcapihttphostname`
 HTTPSURL=`cat kcapihttpshostname`
-echo "Setting $URL as the hostname"
 
 sed -i "s/KATACODE_MGT_HOST/$MGTURL/g" wso2am-ga.sh
 sed -i "s/KATACODE_API_HTTP_HOST/$HTTPURL/g" wso2am-ga.sh
 sed -i "s/KATACODE_API_HTTPS_HOST/$HTTPSURL/g" wso2am-ga.sh
+
+echo "Setting the hostnames"
