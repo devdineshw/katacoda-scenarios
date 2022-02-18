ff2259646d13df14edd035d68b9f4deb027b52a4
dev.dneshw@gmail.com <dev.dineshw@gmail.com>
2022-02-18 16:22:08 +0530

    adding custom handler scenario

--- WSO2-APIM-3.2.0-Lab08-custom-handler/findhostname.sh
+++ WSO2-APIM-3.2.0-Lab08-custom-handler/findhostname.sh
@@ -3,4 +3,3 @@
 echo "[[HOST_SUBDOMAIN]]" >> ~/kcsubdomain
 echo "[[KATACODA_HOST]]" >> ~/kchostname
 
-tar xf apache-maven-3.2.1-bin.tar.gz
---
+++ WSO2-APIM-3.2.0-Lab08-custom-handler/step2-script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+tar xf apache-maven-3.2.1-bin.tar.gz
+
+export PATH='/root/apache-maven-3.2.1/bin:$PATH'
