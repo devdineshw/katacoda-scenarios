09d8d350220e5a9084ad6e2e7f156c599786a519
dev.dneshw@gmail.com <dev.dineshw@gmail.com>
2022-02-21 12:47:09 +0530

    adding UI customization

---
+++ WSO2-APIM-3.2.0-Lab09-customize-ui/findhostname.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "[[HOST_SUBDOMAIN]]" >> ~/kcsubdomain
+echo "[[KATACODA_HOST]]" >> ~/kchostname
+
---
+++ WSO2-APIM-3.2.0-Lab09-customize-ui/step2-script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+tar xf apache-maven-3.2.1-bin.tar.gz
+
+export PATH=$PATH:/root/apache-maven-3.2.1/bin
