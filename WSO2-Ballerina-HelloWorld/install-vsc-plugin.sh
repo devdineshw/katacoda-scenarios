8c4bf0b26bb021ac6f080985f0066341999982f3
dev.dneshw@gmail.com <dev.dineshw@gmail.com>
2022-03-14 22:09:25 +0530

    Adding ballerina hello world

--- WSO2-Ballerina-HelloWorld/install-vsc-plugin.sh
+++ WSO2-Ballerina-HelloWorld/install-vsc-plugin.sh
@@ -1,4 +1,3 @@
 echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selections
 cd /tmp && apt-get update -y && apt install libarchive-tools -y # install bsdtar
-curl -L https://marketplace.visualstudio.com/_apis/public/gallery/publishers/WSO2/vsextensions/ballerina/3.0.0/vspackage | bsdtar -xvf - extension
-mv extension /opt/.katacodacode/extensions/WSO2.ballerina-3.0.0
+curl -L https://marketplace.visualstudio.com/_apis/public/gallery/publishers/WSO2/vsextensions/ballerina/3.0.0/vspackage --output bal-plagin | bsdtar -xvPf bal-plagin -C /opt/.katacodacode/extensions/WSO2.ballerina-3.0.0
